6e7e1aeb04384cdbdfa9d461c86d98a34ff5e2da
Mikiyas <mymikiaa11@gmail.com>
2024-12-08 11:41:06 +0300

    workbox es module change

---
+++ workbox-config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  globDirectory: '.svelte-kit/output/client/',
+  globPatterns: [
+    '**/*.{html,json,js,css,svg,png,ico,txt}'
+  ],
+  swDest: '.svelte-kit/output/client/service-worker.js',
+  clientsClaim: true,
+  skipWaiting: true,
+  // Don't cache Google Fonts and other dynamic resources
+  navigateFallback: '/index.html',
+  navigateFallbackDenylist: [/^\/api\//],
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/www\.youtube\.com\//,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'youtube-cache',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        }
+      }
+    }
+  ]
+};
